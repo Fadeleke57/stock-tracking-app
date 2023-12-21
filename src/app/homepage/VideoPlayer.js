c228d96d44fa46ab4c699a3b0dc993332ea7bd41
Farouk Adeleke <110058327+Fadeleke57@users.noreply.github.com>
2023-12-21 12:18:24 -0500

    Update VideoPlayer.js

--- src/app/homepage/VideoPlayer.js
+++ src/app/homepage/VideoPlayer.js
@@ -1,11 +1,12 @@
 // components/VideoPlayer.js
 import React from 'react';
 import dynamic from "next/dynamic";
+import LandingVid from "../../public/bonsai_landingvidOldWstrnFitted.mp4"
 
 const VideoPlayer = () => {
   return (
     <video muted autoplay>
-      <source src="../../public/bonsai_landingvidOldWstrnFitted.mp4" type="video/mp4" />
+      <source src={LandingVid} type="video/mp4" />
       {/* Add more <source> tags for other video formats if needed */}
       Your browser does not support the video tag.
     </video>
